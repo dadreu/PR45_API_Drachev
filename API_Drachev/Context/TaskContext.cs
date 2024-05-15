fbc375aac28f730fb0f1c7a7c8e927b9ba3c8007
Dadreu <dadreu@gmail.com>
2024-05-15 21:57:34 +0500

    Разработанный контекст для загрузки данных. Драчёв

---
+++ API_Drachev/Context/TaskContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using API_Drachev.Model;
+
+namespace API_Drachev.Context
+{
+    public class TaskContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public TaskContext()
+        {
+            Database.EnsureCreated();
+            Tasks.Load();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1; " +
+                "uid=root;" +
+                "pwd=;database=TaskManagers", 
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
