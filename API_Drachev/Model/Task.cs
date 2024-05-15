c664f44b5de4a542eea44601372c0f6e773e0897
Dadreu <dadreu@gmail.com>
2024-05-15 21:53:17 +0500

    Разработанные модели данных. Драчёв

---
+++ API_Drachev/Model/Task.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace API_Drachev.Model
+{
+    /// <summary>
+    /// Задачи
+    /// </summary>
+    public class Task
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Приоритет
+        /// </summary>
+        public string Priority { get; set; }
+        /// <summary>
+        /// Дата выполнения
+        /// </summary>
+        public DateTime DateExecute { get; set; }
+        /// <summary>
+        /// Описание
+        /// </summary>
+        public string Comment { get; set; }
+        /// <summary>
+        /// Выполнение
+        /// </summary>
+        public bool Done { get; set; }
+    }
+}
