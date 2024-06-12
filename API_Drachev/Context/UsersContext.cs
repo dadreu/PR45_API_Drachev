9efe2151e7f1d7ee46457f42136713312517ee19
Dadreu <dadreu@gmail.com>
2024-06-12 16:09:00 +0500

    Создание модели и контекста пользователя. Drachev

---
+++ API_Drachev/Context/UsersContext.cs
@@ -0,0 +1,24 @@
+﻿using API_Drachev.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace API_Drachev.Context
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<Users> Users {  get; set; }
+        UsersContext() 
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql("server=127.0.0.1; " +
+                "uid=root;" +
+                "pwd=;" +
+                "database=TaskManagers",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+
+    }
+}
+}
