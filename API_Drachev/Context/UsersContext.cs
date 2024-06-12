e7d8676543aeec08f4047229e522dd355be6f50a
Dadreu <dadreu@gmail.com>
2024-06-12 23:12:37 +0500

    Разработанный метод регистрации пользователя. Drachev

--- API_Drachev/Context/UsersContext.cs
+++ API_Drachev/Context/UsersContext.cs
@@ -12,13 +12,9 @@ namespace API_Drachev.Context
             Database.EnsureCreated();
             Users.Load();
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-            optionsBuilder.UseMySql("server=127.0.0.1; " +
-                "uid=root;" +
-                "pwd=;" +
-                "database=TaskManagers",
-                new MySqlServerVersion(new Version(8, 0, 11)));
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+            optionsBuilder.UseMySql("server=localhost;uid=root;database=TaskManager", 
+                new MySqlServerVersion(new System.Version(8, 0, 11)));
     }
 }
-}
+
--- API_Drachev/Controllers/UsersController.cs
+++ API_Drachev/Controllers/UsersController.cs
@@ -26,17 +26,51 @@ namespace API_Drachev.Controllers
         [ProducesResponseType(500)]
         public ActionResult SingIn([FromForm] string Login, [FromForm] string Password)
         {
-            if (Login == null || Password == null) 
+            if (Login == null || Password == null)
                 return StatusCode(403);
             try
             {
-                Users User = new UsersContext().Users.Where(x => x.Login == Login && 
+                Users User = new UsersContext().Users.Where(x => x.Login == Login &&
                 x.Password == Password).First;
                 return Json(User);
             }
             catch (Exception ex)
             {
-                return StatusCode(500);
+                return StatusCode(500, ex.Message);
+            }
+        }
+        /// <summary>
+        /// Регистрация нового пользователя
+        /// </summary>
+        /// <param name="Login">Логин</param>
+        /// <param name="Password">Пароль</param>
+        /// <returns>Данный метод предназначен для регистрации пользователя на сайте</returns>
+        /// /// <response code="200">Пользователь успешно зарегистрирован</response>
+        /// /// <response code="403">Ошибка запроса, данные не указаны</response>
+        /// /// <response code="500">При выполнении запроса возникла ошибка</response>
+        [Route("RegIn")]
+        [HttpPost]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(500)]
+        public ActionResult RegIn([FromForm] string Login, [FromForm] string Password)
+        {
+            if (Login == null || Password == null) return StatusCode(403);
+            try
+            {
+                var newUser = new UsersContext();
+                Users User = new Users()
+                {
+                    Login = Login,
+                    Password = Password
+                };
+                newUser.Users.Add(User);
+                newUser.SaveChanges();
+                return Json(newUser);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
             }
         }
     }
