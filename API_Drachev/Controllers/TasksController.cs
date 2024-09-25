7d5d997d910d3f7f41380cd26d6987d6787ddc79
dadreu <dadreu@gmail.com>
2024-09-26 00:26:03 +0500

    Разработка запроса на обновление. Драчёв

--- API_Drachev/Controllers/TasksController.cs
+++ API_Drachev/Controllers/TasksController.cs
@@ -13,7 +13,14 @@ namespace API_Drachev.Controllers
     public class TaskContoller : Controller
     {
 
-            [Route("List")]
+        /// <summary>
+        /// Получение списка задач
+        /// </summary>
+        /// <remarks>Данный метод получает список задач, находящийся в БД</remarks>
+        /// <response code="200">Задача успешно получена</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        /// <returns></returns>    
+        [Route("List")]
             [HttpGet]
             [ProducesResponseType(typeof(List<Task>), 200)]
             [ProducesResponseType(500)]
@@ -30,6 +37,12 @@ namespace API_Drachev.Controllers
                 }
             }
 
+            /// <summary>
+            /// Получение задачи
+            /// </summary>
+            /// <remarks>Данный метод получает задачу, находящуюся в БД</remarks>
+            /// <response code="200">Задача успешно получена</response>
+            /// <response code="500">При выполнении запроса возникли ошибки</response>
             [Route("Item")]
             [HttpGet]
             [ProducesResponseType(typeof(Task), 200)]
@@ -70,6 +83,46 @@ namespace API_Drachev.Controllers
                     return StatusCode(500, exp.Message);
                 }
             }
+        ///<summary>
+        ///Метод изменения задачи
+        /// </summary>
+        /// <param name="=task">Данные о задаче</param>
+        /// <returns>Статус выполнения запроса</returns>
+        ///<remarks>Данный метод изменяет задачу в базе данных</remarks>
+        [Route("Edit")]
+        [HttpPut]
+        [ApiExplorerSettings(GroupName = "v3")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public ActionResult Edit([FromForm] Task task)
+        {
+            try
+            {
+                TaskContext taskContext = new TaskContext();
+                var eTask = taskContext.Tasks.SingleOrDefault(x => x.Id == task.Id);
+                if (eTask != null)
+                {
+                    eTask.Name = task.Name;
+                    eTask.Priority = task.Priority;
+                    eTask.DateExecute = task.DateExecute;
+                    eTask.Comment = task.Comment;
+                    eTask.Done = task.Done;
+                    taskContext.SaveChanges();
+                    return StatusCode(200);
+                }
+                else
+                {
+                    return StatusCode(400);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
         }
+
+    }
+
     }
 
